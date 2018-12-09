026a6482fef21285764792a82c005bd768f7d8fa
rajan <rajangreedge@gmail.com>
2018-12-09 11:48:56 +0530

    #access image alt attr

--- js/main.js
+++ js/main.js
@@ -160,11 +160,13 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.setAttribute("alt", restaurant.name + " restaurant presentational image");
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+
   li.append(name);
 
   const neighborhood = document.createElement('p');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,6 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.setAttribute("alt", restaurant.name + " restaurant presentational image");
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
